884368f79ee9f001f0df14c7d67cf6cc21d42dda
Ryan Hinkel <ryanhinkel@gmail.com>
2016-06-13 17:14:43 -0400

    Cheaper source maps

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,24 @@
-module.exports = {
-    entry: "./src/js/app.js.coffee",
-    output: {
-        path: __dirname,
-        filename: "bundle.js"
-    },
-    resolve: {
-        extensions: ["", ".webpack.js", ".web.js", ".js", ".js.coffee"]
-    },
-    devtool: "#inline-source-map",
-    module: {
-        loaders: [
-            { test: /\.coffee$/, loader: "coffee-loader" }
-        ]
-    }
-};
+var webpack = require('webpack');
+
+var env = process.env.WEBPACK_ENV
+var name = 'style-tools'
+
+config = {}
+config.module =  {
+    loaders: [
+        { test: /\.coffee$/, loader: "coffee-loader" }
+    ]
+}
+config.resolve = {
+    extensions: ["", ".webpack.js", ".web.js", ".js", ".js.coffee"]
+}
+
+
+config.entry = "./src/js/app.js.coffee";
+config.output = {
+    path: __dirname,
+    filename: 'bundle.js' }
+
+config.devtool = "cheap-module-source-map"
+
+module.exports = config
